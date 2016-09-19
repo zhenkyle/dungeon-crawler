ae31c8e6dfb4a7f3b2b5f81f4788c89de9de5f05
Zhenkyle <zhenkyle@gmail.com>
2016-09-19 16:06:21 +0800

    Initial Commit

---
+++ conf/karma-files.conf.js
@@ -0,0 +1,32 @@
+const conf = require('./gulp.conf');
+const wiredep = require('wiredep');
+
+module.exports = function listFiles() {
+  const wiredepOptions = Object.assign({}, conf.wiredep, {
+    overrides: {
+      react: {main: ['react-with-addons.js', 'react-dom.js']}
+    },
+    dependencies: true,
+    devDependencies: true
+  });
+
+  const patterns = wiredep(wiredepOptions).js.concat([
+    'node_modules/es6-shim/es6-shim.js',
+    `!${conf.path.tmp('**/*.spec.js')}`,
+    conf.path.tmp('app/constants/*.js'),
+    conf.path.tmp('app/reducers/todos.js'),
+    conf.path.tmp('app/reducers/index.js'),
+    conf.path.tmp('app/actions/index.js'),
+    conf.path.tmp('app/components/*.js'),
+    `${conf.path.tmp('**/*.spec.js')}`
+  ]);
+
+  const files = patterns.map(pattern => ({pattern}));
+  files.push({
+    pattern: conf.path.src('assets/**/*'),
+    included: false,
+    served: true,
+    watched: false
+  });
+  return files;
+};
---
+++ gulp_tasks/inject.js
@@ -0,0 +1,31 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync');
+const wiredep = require('wiredep').stream;
+const gulpInject = require('gulp-inject');
+
+const conf = require('../conf/gulp.conf');
+
+gulp.task('inject', inject);
+
+function inject() {
+  const injectScripts = gulp.src([
+    conf.path.tmp('app/constants/*.js'),
+    conf.path.tmp('app/reducers/todos.js'),
+    conf.path.tmp('app/reducers/index.js'),
+    conf.path.tmp('app/actions/index.js'),
+    conf.path.tmp('**/!(index).js'),
+    conf.path.tmp('**/index.js'),
+    `!${conf.path.tmp('**/*.spec.js')}`
+  ]);
+
+  const injectOptions = {
+    ignorePath: [conf.paths.src, conf.paths.tmp],
+    addRootSlash: false
+  };
+
+  return gulp.src(conf.path.src('index.html'))
+    .pipe(gulpInject(injectScripts, injectOptions))
+    .pipe(wiredep(Object.assign({}, conf.wiredep)))
+    .pipe(gulp.dest(conf.paths.tmp))
+    .pipe(browserSync.stream());
+}
